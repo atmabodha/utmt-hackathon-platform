afddb5489ebef7a72f0a0d376ccad8e35ac65abb
pankajyadav8523 <py9621008569@gmail.com>
2024-07-16 10:12:41 +0530

    fixing the blinking effect

--- front-end/coding_contest_app/src/Router.js
+++ front-end/coding_contest_app/src/Router.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ContestHistory from "./host/dashboard/Dashboard";
+// import ContestHistory from "./host/dashboard/Dashboard";
 import Leaderboard from "./host/leaderboard/Leaderboard";
-import ContestDetails from "./host/contest creation/ContestDetails";
+// import ContestDetails from "./host/contest creation/ContestDetails";
 
 function ApplicationRouter() {
   return (
     <Router>
       <Routes>
         {/* <Route path="/" element={<ContestHistory />} /> */}
-        <Route path="/" element={<ContestDetails />} />
+        {/* <Route path="/" element={<ContestDetails />} /> */}
         <Route path="/leaderboard" element={<Leaderboard />} />
         {/* <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} /> */}
