d934bd43fef3f5be784752d7456dc984ee95ca99
narayan-jat <narayanjat2964@gmail.com>
2024-07-19 21:43:23 +0530

    nothing special

--- front-end/coding_contest_app/src/Router.js
+++ front-end/coding_contest_app/src/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import ContestHistory from "./host/dashboard/Dashboard";
+import ContestHistory from "./host/dashboard/Dashboard";
 import Leaderboard from "./host/leaderboard/Leaderboard";
-// import ContestDetails from "./host/contest creation/ContestDetails";
+import ContestDetails from "./host/contest creation/ContestDetails";
 
 function ApplicationRouter() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<ContestHistory />} /> */}
+        <Route path="/" element={<ContestHistory />} />
         {/* <Route path="/" element={<ContestDetails />} /> */}
         <Route path="/leaderboard" element={<Leaderboard />} />
         {/* <Route path="/about" element={<About />} />
