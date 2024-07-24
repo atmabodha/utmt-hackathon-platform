4f7968e5fffc37950bedd42d67707e2fff95efaf
pankajyadav8523 <py9621008569@gmail.com>
2024-07-24 16:50:25 +0530

    adding pagination effect

--- front-end/coding_contest_app/src/host/components/leaderboard/Leaderboard.js
+++ front-end/coding_contest_app/src/host/components/leaderboard/Leaderboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
 import "./Leaderboard.css";
 import profile1 from "./leaderboard assets/profile1.jpeg"; // Corrected the path
 
@@ -23,9 +24,54 @@ const data = [
   { rank: 6, name: "Robert Fox", points: 890, prize: "$0", img: profile1 },
   { rank: 7, name: "Bessie Cooper", points: 880, prize: "$0", img: profile1 },
   { rank: 8, name: "Jerome Bell", points: 790, prize: "$0", img: profile1 },
+  { rank: 9, name: "Courtney Henry", points: 960, prize: "$0", img: profile1 },
+  {
+    rank: 10,
+    name: "Marvin McKinney",
+    points: 940,
+    prize: "$0",
+    img: profile1,
+  },
+  { rank: 11, name: "Robert Fox", points: 890, prize: "$0", img: profile1 },
+  { rank: 12, name: "Bessie Cooper", points: 880, prize: "$0", img: profile1 },
+  { rank: 13, name: "Jerome Bell", points: 790, prize: "$0", img: profile1 },
+  { rank: 14, name: "Courtney Henry", points: 960, prize: "$0", img: profile1 },
+  {
+    rank: 15,
+    name: "Marvin McKinney",
+    points: 940,
+    prize: "$0",
+    img: profile1,
+  },
+  { rank: 16, name: "Robert Fox", points: 890, prize: "$0", img: profile1 },
+  { rank: 17, name: "Bessie Cooper", points: 880, prize: "$0", img: profile1 },
+  { rank: 18, name: "Jerome Bell", points: 790, prize: "$0", img: profile1 },
+  { rank: 19, name: "Courtney Henry", points: 960, prize: "$0", img: profile1 },
+  {
+    rank: 20,
+    name: "Marvin McKinney",
+    points: 940,
+    prize: "$0",
+    img: profile1,
+  },
+  { rank: 21, name: "Robert Fox", points: 890, prize: "$0", img: profile1 },
+  { rank: 22, name: "Bessie Cooper", points: 880, prize: "$0", img: profile1 },
+  { rank: 23, name: "Jerome Bell", points: 790, prize: "$0", img: profile1 },
+  // Add more items if needed
 ];
 
-const Leaderboard = () => {
+const Leaderboard = ({ itemsPerPage = 6 }) => {
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = data.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    setItemOffset(newOffset);
+  };
+
   return (
     <div className="leaderboard">
       <h1 className="title">LEADERBOARD</h1>
@@ -39,7 +85,7 @@ const Leaderboard = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((player, index) => (
+          {currentItems.map((player, index) => (
             <tr className="tableRow" key={index}>
               <td className="tableData">{player.rank}</td>
               <td className="tableData" id="player-info">
@@ -52,6 +98,21 @@ const Leaderboard = () => {
           ))}
         </tbody>
       </table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName="pagination"
+        activeClassName="active"
+        pageLinkClassName="page-link"
+        previousLinkClassName="page-link"
+        nextLinkClassName="page-link"
+        breakLinkClassName="page-link"
+      />
     </div>
   );
 };
