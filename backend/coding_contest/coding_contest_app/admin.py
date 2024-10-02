38d443f132b3b69fcf555ab583a4c5bc1e34b9b2
pankajyadav8523 <py9621008569@gmail.com>
2024-10-02 22:12:39 +0530

    adding all the models to the admin site

--- backend/coding_contest/coding_contest_app/admin.py
+++ backend/coding_contest/coding_contest_app/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contests, ContestDetails, ContestPrizes, ContestRegistration, Users
+from .models import Contests, ContestDetails, ContestPrizes, ContestRegistration, Users, SocialMediaLink, Samples
+from .models import UserDetails, UserAddress, Problems, ContestProblems, SubmissionResult, Submissions, Ranking, Language, TestCases
 
 admin.site.register(Contests)
 admin.site.register(ContestRegistration)
 admin.site.register(ContestDetails)
 admin.site.register(ContestPrizes)
 admin.site.register(Users)
+admin.site.register(SocialMediaLink)
+admin.site.register(Samples)
+admin.site.register(UserAddress)
+admin.site.register(UserDetails)
+admin.site.register(Problems)
+admin.site.register(ContestProblems)
+admin.site.register(SubmissionResult)
+admin.site.register(Submissions)
+admin.site.register(Ranking)
+admin.site.register(Language)
+admin.site.register(TestCases)
