a589b48ca9753bab42a687fba2784188775702c5
pankajyadav8523 <py9621008569@gmail.com>
2024-10-03 20:21:33 +0530

    fixing conflict of router.jsx

--- front-end/coding_contest_app/src/Router.jsx
+++ front-end/coding_contest_app/src/Router.jsx
@@ -11,22 +11,15 @@ import ContestRegistration from "./host/components/contest creation/ContestRegis
 import Challenge from "./host/components/contest creation/Challenge.jsx";
 import Prizes from "./host/components/contest creation/Prizes.jsx";
 import QuestionsPage from "./participant/pages/QuestionsPage.jsx";
-<<<<<<< HEAD
 import Login from "./host/components/login/Login.jsx";
-=======
 import Signup from "./host/components/signup/Signup.jsx";
->>>>>>> dcaaf5ff22f3a3127cd9ac9ebf49ac6b8d8f84ce
 
 function ApplicationRouter() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Login />} />
-=======
-        <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
->>>>>>> dcaaf5ff22f3a3127cd9ac9ebf49ac6b8d8f84ce
         <Route path="/leaderboard" element={<LeaderboardPage />} />
         <Route path="/host" element={<ContestCreation />} />
         <Route path="/submissions" element={<ContestDetails />} />
