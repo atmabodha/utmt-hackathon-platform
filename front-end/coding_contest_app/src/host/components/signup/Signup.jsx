83ac11ce4ae9656b42e5f5360bb70f6c315b7117
shravanbishnoi <shravanbishnoi008754@gmail.com>
2024-10-04 20:42:10 +0530

    Make Signup page responsive for all device

--- front-end/coding_contest_app/src/host/components/signup/Signup.jsx
+++ front-end/coding_contest_app/src/host/components/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {TextInputField} from '../../../utilities/FormComponents';
-import { useNavigate, Link } from 'react-router-dom';
+import {useNavigate, Link} from 'react-router-dom';
 import './Signup.css';
 import {Button} from 'react-bootstrap';
 import {Form} from 'react-bootstrap';
@@ -8,24 +8,24 @@ import AuthContext from '../../../context/AuthContext';
 import {useFormHandler} from '../contest creation/FormHandlers';
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const { user, signup } = useContext(AuthContext)
+  const navigate = useNavigate ();
+  const {user, signup} = useContext (AuthContext);
   const {formData: signUpData, handleInputChange} = useFormHandler ({
     name: '',
     email: '',
     password: '',
   });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault ();
     try {
-      await signup(signUpData.email, signUpData.password, signUpData.name);
-      navigate('/host')
-      console.log("user data: ", user)
+      await signup (signUpData.email, signUpData.password, signUpData.name);
+      navigate ('/host');
+      console.log ('user data: ', user);
     } catch (e) {
-      console.log(e)
+      console.log (e);
     }
-};
+  };
 
   const signUpFields = [
     {
@@ -52,36 +52,34 @@ const SignUp = () => {
   ];
 
   return (
-    <div className="signup-page">
-      <div className="signup-form">
-        <div>
-          <h1>Code Hut</h1>
-          <h2>Sign Up</h2>
-        </div>
-        <div className="signup-input-fields">
-          <Form>
-            {signUpFields.map ((field, index) => (
-              <TextInputField
-                key={index}
-                label={field.label}
-                name={field.name}
-                type={field.type}
-                value={signUpData[field.name]}
-                onChange={handleInputChange}
-                groupClass={'signup-input-field'}
-                placeholder={field.placeholder}
-                controlClass={'input-field'}
-              />
-            ))}
-          </Form>
-        </div>
-        <div className="signup-form-footer">
-          <Button className="signup-btn" onClick={handleSubmit}>Submit</Button>
-          <div className="signup-footer-text">
-            <p className="signup-footer-text">
-              Already have an account? <Link to="/">Log in</Link>
-            </p>
-          </div>
+    <div className="signup-form">
+      <div>
+        <h1>Code Hut</h1>
+        <h2>Sign Up</h2>
+      </div>
+      <div className="signup-input-fields">
+        <Form>
+          {signUpFields.map ((field, index) => (
+            <TextInputField
+              key={index}
+              label={field.label}
+              name={field.name}
+              type={field.type}
+              value={signUpData[field.name]}
+              onChange={handleInputChange}
+              groupClass={'signup-input-field'}
+              placeholder={field.placeholder}
+              controlClass={'input-field'}
+            />
+          ))}
+        </Form>
+      </div>
+      <div className="signup-form-footer">
+        <Button className="signup-btn" onClick={handleSubmit}>Submit</Button>
+        <div className="signup-footer-text">
+          <p className="signup-footer-text">
+            Already have an account? <Link to="/">Log in</Link>
+          </p>
         </div>
       </div>
     </div>
