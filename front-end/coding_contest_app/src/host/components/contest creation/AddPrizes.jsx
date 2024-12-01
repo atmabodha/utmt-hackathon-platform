75307507bacee0cd4bc8359e3bc9fa80fcbebc0b
shravanbishnoi <shravanbishnoi008754@gmail.com>
2024-12-01 18:12:56 +0530

    Redirect prizes fix

--- front-end/coding_contest_app/src/host/components/contest creation/AddPrizes.jsx
+++ front-end/coding_contest_app/src/host/components/contest creation/AddPrizes.jsx
@@ -1,64 +1,71 @@
-import React from "react";
-import "./AddPrizes.css";
-import { Link } from "react-router-dom";
-import { Form } from "react-bootstrap";
-import ContestEditFooter from "./ContestEditFooter";
-import { useFormHandler } from "./FormHandlers";
-import TextAreaField, {
-  TextInputField,
-} from "../../../utilities/FormComponents";
-import { BASE_SERVER_URL, CONTESTS, HOST_ENDPOINT } from "../../../Constants";
-import { useParams } from "react-router-dom";
-import showSwalAlert from "../../../utilities/AlertComponents.jsx";
-import { sendData } from "../../apis/ApiRequests";
+import React from 'react';
+import './AddPrizes.css';
+import {Link} from 'react-router-dom';
+import {Form} from 'react-bootstrap';
+import ContestEditFooter from './ContestEditFooter';
+import {useFormHandler} from './FormHandlers';
+import TextAreaField, {TextInputField} from '../../../utilities/FormComponents';
+import {BASE_SERVER_URL, CONTESTS, HOST_ENDPOINT} from '../../../Constants';
+import {useParams, useNavigate} from 'react-router-dom';
+import showSwalAlert from '../../../utilities/AlertComponents.jsx';
+import {sendData} from '../../apis/ApiRequests';
 
-function AddPrizes({ contestUrl }) {
-  const { contestId } = useParams();
+function AddPrizes({contestUrl}) {
+  const {contestId} = useParams ();
+  const navigate = useNavigate ();
   const textAreaFields = [
     {
-      label: "Prize Description",
-      name: "prizeDescription",
+      label: 'Prize Description',
+      name: 'prizeDescription',
     },
     {
-      label: "Others",
-      name: "others",
+      label: 'Others',
+      name: 'others',
     },
   ];
 
   const inputFields = [
     {
-      label: "Prize Position",
-      name: "prizePosition",
-      type: "text",
+      label: 'Prize Position',
+      name: 'prizePosition',
+      type: 'text',
     },
 
     {
-      label: "Prize Amount",
-      name: "prizeAmount",
-      type: "number",
+      label: 'Prize Amount',
+      name: 'prizeAmount',
+      type: 'number',
     },
   ];
-  const { formData: prizeData, handleInputChange } = useFormHandler({
-    prizePosition: "",
-    prizeDescription: "",
-    prizeAmount: "",
-    others: "",
+  const {formData: prizeData, handleInputChange} = useFormHandler ({
+    prizePosition: '',
+    prizeDescription: '',
+    prizeAmount: '',
+    others: '',
   });
 
   const handlePrizeSubmit = async () => {
-    const prizeFormData = new FormData();
-    prizeFormData.append("prize_position", prizeData.prizePosition);
-    prizeFormData.append("prize_description", prizeData.prizeDescription);
-    prizeFormData.append("prize_amount", prizeData.prizeAmount);
-    prizeFormData.append("others", prizeData.others);
-    prizeFormData.append("contest", contestId);
-    const url = BASE_SERVER_URL + HOST_ENDPOINT + CONTESTS + contestId + "/edit/prizes/";
-    const response = await sendData(url, prizeFormData);
+    const prizeFormData = new FormData ();
+    prizeFormData.append ('prize_position', prizeData.prizePosition);
+    prizeFormData.append ('prize_description', prizeData.prizeDescription);
+    prizeFormData.append ('prize_amount', prizeData.prizeAmount);
+    prizeFormData.append ('others', prizeData.others);
+    prizeFormData.append ('contest', contestId);
+    const url =
+      BASE_SERVER_URL + HOST_ENDPOINT + CONTESTS + contestId + '/edit/prizes/';
+    const response = await sendData (url, prizeFormData);
     if (response) {
-      showSwalAlert({
-        icon: "success",
-        title: "Prize added",
-        text: "Prizes details updated.",
+      showSwalAlert ({
+        icon: 'success',
+        title: 'Prize added',
+        text: 'Prizes details updated.',
+      });
+      navigate (`/administration/contests/${contestId}/edit/prizes`);
+    } else {
+      showSwalAlert ({
+        icon: 'error',
+        title: 'Unable to add',
+        text: 'Please try again',
       });
     }
   };
@@ -67,17 +74,17 @@ function AddPrizes({ contestUrl }) {
     <div>
       <div className="prize-contest">
         <div className="prize-contest-header">
-          <h2 style={{ fontWeight: 600 }}>Add Prize</h2>
+          <h2 style={{fontWeight: 600}}>Add Prize</h2>
           <Link
             to=""
-            style={{ textDecoration: "none", color: "var(--text-color)" }}
+            style={{textDecoration: 'none', color: 'var(--text-color)'}}
           >
             {contestUrl}www.codehut.com/hackHard
           </Link>
         </div>
         <div className="prize-form">
           <Form>
-            {inputFields.map((field, index) => (
+            {inputFields.map ((field, index) => (
               <TextInputField
                 key={index}
                 type={field.type}
@@ -86,13 +93,13 @@ function AddPrizes({ contestUrl }) {
                 value={prizeData[field.name]}
                 onChange={handleInputChange}
                 controlId={`ControlTextarea${index}`}
-                groupClass={"prize-input-field"}
-                labelClass={"text-area-label"}
-                controlClass={"text-area-control"}
+                groupClass={'prize-input-field'}
+                labelClass={'text-area-label'}
+                controlClass={'text-area-control'}
               />
             ))}
 
-            {textAreaFields.map((field, index) => (
+            {textAreaFields.map ((field, index) => (
               <TextAreaField
                 key={index}
                 label={field.label}
@@ -100,9 +107,9 @@ function AddPrizes({ contestUrl }) {
                 value={prizeData[field.name]}
                 onChange={handleInputChange}
                 controlId={`ControlTextarea${index}`}
-                groupClass={"prize-input-field"}
-                labelClass={"text-area-label"}
-                controlClass={"text-area-control"}
+                groupClass={'prize-input-field'}
+                labelClass={'text-area-label'}
+                controlClass={'text-area-control'}
               />
             ))}
           </Form>
