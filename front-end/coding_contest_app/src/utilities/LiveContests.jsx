a84b42982d0221d7d02cc6bdb118cffcf2e92629
pankajyadav8523 <py9621008569@gmail.com>
2024-12-03 13:20:41 +0530

    connecting contest details

--- front-end/coding_contest_app/src/utilities/LiveContests.jsx
+++ front-end/coding_contest_app/src/utilities/LiveContests.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaUsers, FaClock } from "react-icons/fa";
-import { MonthFormattedDate, TimeDifference, TimeFromDate } from "./TimeConversion";
+import {
+  MonthFormattedDate,
+  TimeDifference,
+  TimeFromDate,
+} from "./TimeConversion";
 import "./styles/Contest.css";
 
 const LiveContests = ({ data }) => {
@@ -27,7 +31,8 @@ const LiveContests = ({ data }) => {
                 <h5 className="card-title">{contest.contest_name}</h5>
                 <p className="card-text">
                   Contest Started on{" "}
-                  <MonthFormattedDate utcTime={contest.start_date_time} /> at <TimeFromDate utcTime={contest.start_date_time}/>
+                  <MonthFormattedDate utcTime={contest.start_date_time} /> at{" "}
+                  <TimeFromDate utcTime={contest.start_date_time} />
                 </p>
                 <div className="d-flex justify-content-between">
                   <div className="d-flex ">
@@ -39,12 +44,21 @@ const LiveContests = ({ data }) => {
                   <div className="d-flex ">
                     <FaClock size={20} />
                     <p className="ms-1" style={{ fontSize: "smaller" }}>
-                      Time: <TimeDifference startDate={contest.start_date_time} endDate={contest.end_date_time} />
+                      Time:{" "}
+                      <TimeDifference
+                        startDate={contest.start_date_time}
+                        endDate={contest.end_date_time}
+                      />
                     </p>
                   </div>
                 </div>
                 <div className="details mt-2">
-                  <Link className="btn" id="link-button">
+                  <Link
+                    className="btn"
+                    id="link-button"
+                    to={`/contests/${contest.contest_id}/details`}
+                    target="_blank"
+                  >
                     Contest Details
                   </Link>
                 </div>
